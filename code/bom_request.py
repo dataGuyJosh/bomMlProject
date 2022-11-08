c61e7763893b07e77edcaf5049750dd3f905c08e
Josh Garvey <12745216+dataGuyJosh@users.noreply.github.com>
2022-11-08 20:07:29 +1000

    adding git ignore, understanding that not all Cross Validation methods work well with continuous data

--- code/bom_request.py
+++ code/bom_request.py
@@ -2,11 +2,12 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from io import StringIO
 from sklearn import tree, preprocessing
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import train_test_split, cross_val_score, KFold, StratifiedKFold, LeaveOneOut, LeavePOut, ShuffleSplit
+from sklearn.model_selection import cross_val_score, KFold, StratifiedKFold, LeaveOneOut, LeavePOut, ShuffleSplit
 bom_url = 'http://reg.bom.gov.au/fwo/IDV60801/IDV60801.95936.json'
-df_response = pd.read_json(requests.get(bom_url).text, orient='index')
+df_response = pd.read_json(StringIO(requests.get(bom_url).text), orient='index')
 obs_df = pd.DataFrame(df_response.data.observations)
 
 features = ['delta_t', 'gust_kmh', 'gust_kt', 'dewpt', 'press',
@@ -20,20 +21,27 @@ obs_df['wind_dir'] = le.transform(obs_df['wind_dir'])
 
 obs_df[features] = obs_df[features].astype(float)
 
-x = obs_df[features]
+X = obs_df[features]
 y = obs_df['air_temp']
 
-dt = DecisionTreeRegressor()
-dt = dt.fit(x, y)
+clf = DecisionTreeRegressor()
+clf = clf.fit(X, y)
 
-# tree.plot_tree(dt, feature_names=features)
-# plt.show()
+# # tree.plot_tree(dt, feature_names=features)
+# # plt.show()
 
-x = x.values.tolist()
-y = np.array(y.values.tolist())
+k_fold = KFold(n_splits=10)
+scores = cross_val_score(clf, X, y, cv=k_fold)
+print('K-Fold',
+      '\nIndividual Scores:', scores,
+      '\nAverage Score:', scores.mean(),
+      '\nNumber of scores used in Average:', len(scores))
 
-loo = LeaveOneOut()
-scores = cross_val_score(dt, x, y, cv=loo)
-print('Individual Scores:', scores,
+# Shuffle Split
+ss = ShuffleSplit(train_size=0.6, test_size=0.3, n_splits=10)
+scores = cross_val_score(clf, X, y, cv=ss)
+
+print('\nShuffle Split',
+      '\nIndividual Scores:', scores,
       '\nAverage Score:', scores.mean(),
       '\nNumber of scores used in Average:', len(scores))
