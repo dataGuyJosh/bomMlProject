b186a459034512807300fa17c1a02409a009542f
Josh Garvey <12745216+dataGuyJosh@users.noreply.github.com>
2022-11-08 21:35:15 +1000

    separating logic

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,17 +1,11 @@
-import requests
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-from io import StringIO
-from sklearn import tree, preprocessing, linear_model
-from sklearn.model_selection import cross_val_score, KFold, StratifiedKFold, LeaveOneOut, LeavePOut, ShuffleSplit
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.tree import DecisionTreeRegressor
+from sklearn import preprocessing
+from pull_data import get_request
 
+from sklearn.model_selection import cross_val_score, KFold, StratifiedKFold, LeaveOneOut, LeavePOut, ShuffleSplit
+from sklearn.tree import DecisionTreeRegressor
 bom_url = 'http://reg.bom.gov.au/fwo/IDV60801/IDV60801.95936.json'
-df_response = pd.read_json(
-    StringIO(requests.get(bom_url).text), orient='index')
-obs_df = pd.DataFrame(df_response.data.observations)
+
+obs_df = get_request(bom_url)
 
 features = ['delta_t', 'gust_kmh', 'gust_kt', 'dewpt', 'press',
             'press_msl', 'press_qnh', 'rain_trace', 'rel_hum',
@@ -29,11 +23,6 @@ y = obs_df['air_temp']
 
 dt_clf = DecisionTreeRegressor()
 dt_clf = dt_clf.fit(X, y)
-
-# # https://stackoverflow.com/questions/10988082/multivariate-polynomial-regression-with-numpy
-# ply_clf = PolynomialFeatures(degree=2)
-# X_trans = ply_clf.fit_transform(X)
-# y_trans = ply_clf.fit_transform([y])
 
 for clf in [dt_clf]:
     k_fold = KFold(n_splits=10, shuffle=True)
