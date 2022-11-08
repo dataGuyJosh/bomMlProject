0c82041d486c3b7421185dc998e7c674a2fb0ff3
Josh Garvey <12745216+dataGuyJosh@users.noreply.github.com>
2022-11-08 21:13:08 +1000

    DT with K-Fold working

--- code/bom_request.py
+++ code/bom_request.py
@@ -3,11 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from io import StringIO
-from sklearn import tree, preprocessing
-from sklearn.tree import DecisionTreeRegressor
+from sklearn import tree, preprocessing, linear_model
 from sklearn.model_selection import cross_val_score, KFold, StratifiedKFold, LeaveOneOut, LeavePOut, ShuffleSplit
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.tree import DecisionTreeRegressor
+
 bom_url = 'http://reg.bom.gov.au/fwo/IDV60801/IDV60801.95936.json'
-df_response = pd.read_json(StringIO(requests.get(bom_url).text), orient='index')
+df_response = pd.read_json(
+    StringIO(requests.get(bom_url).text), orient='index')
 obs_df = pd.DataFrame(df_response.data.observations)
 
 features = ['delta_t', 'gust_kmh', 'gust_kt', 'dewpt', 'press',
@@ -24,24 +27,18 @@ obs_df[features] = obs_df[features].astype(float)
 X = obs_df[features]
 y = obs_df['air_temp']
 
-clf = DecisionTreeRegressor()
-clf = clf.fit(X, y)
+dt_clf = DecisionTreeRegressor()
+dt_clf = dt_clf.fit(X, y)
 
-# # tree.plot_tree(dt, feature_names=features)
-# # plt.show()
+# # https://stackoverflow.com/questions/10988082/multivariate-polynomial-regression-with-numpy
+# ply_clf = PolynomialFeatures(degree=2)
+# X_trans = ply_clf.fit_transform(X)
+# y_trans = ply_clf.fit_transform([y])
 
-k_fold = KFold(n_splits=10)
-scores = cross_val_score(clf, X, y, cv=k_fold)
-print('K-Fold',
-      '\nIndividual Scores:', scores,
-      '\nAverage Score:', scores.mean(),
-      '\nNumber of scores used in Average:', len(scores))
-
-# Shuffle Split
-ss = ShuffleSplit(train_size=0.6, test_size=0.3, n_splits=10)
-scores = cross_val_score(clf, X, y, cv=ss)
-
-print('\nShuffle Split',
-      '\nIndividual Scores:', scores,
-      '\nAverage Score:', scores.mean(),
-      '\nNumber of scores used in Average:', len(scores))
+for clf in [dt_clf]:
+    k_fold = KFold(n_splits=10, shuffle=True)
+    scores = cross_val_score(clf, X, y, cv=k_fold)
+    print(clf,
+          '\nIndividual Scores:', scores,
+          '\nAverage Score:', scores.mean(),
+          '\nNumber of scores used in Average:', len(scores))
