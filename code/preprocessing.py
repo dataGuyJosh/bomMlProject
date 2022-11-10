7f6ae836e6b3c19cdda6945efae22a61445c46ca
Josh Garvey <12745216+dataGuyJosh@users.noreply.github.com>
2022-11-10 17:27:32 +1000

    Preprocessing & Refactoring

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,33 +1,39 @@
-from sklearn import preprocessing
+import pandas as pd
+import pandasql as ps
 from pull_data import get_request
-
+from sklearn import preprocessing
+from io import StringIO
 from sklearn.model_selection import cross_val_score, KFold, StratifiedKFold, LeaveOneOut, LeavePOut, ShuffleSplit
 from sklearn.tree import DecisionTreeRegressor
-bom_url = 'http://reg.bom.gov.au/fwo/IDV60801/IDV60801.95936.json'
 
-obs_df = get_request(bom_url)
+bom_url_old = 'http://reg.bom.gov.au/fwo/IDV60801/IDV60801.95936.json'
+bom_url = 'http://reg.bom.gov.au/climate/dwo/202210/text/IDCJDW3049.202210.csv'
 
-features = ['delta_t', 'gust_kmh', 'gust_kt', 'dewpt', 'press',
-            'press_msl', 'press_qnh', 'rain_trace', 'rel_hum',
-            'vis_km', 'wind_dir', 'wind_spd_kmh', 'wind_spd_kt']
+obs_df = pd.read_csv(
+    StringIO(get_request(bom_url)), skiprows=5).iloc[:, 1:]
 
-# label encode wind direction
+
+features_le = ['Direction of maximum wind gust ', '9am wind direction', '3pm wind direction']
+features_ws = ['9am wind speed (km/h)','3pm wind speed (km/h)']
+
+# replace "Calm" with 0 and convert to int
+obs_df[features_ws] = obs_df[features_ws].replace(['Calm'],0).astype(int)
+# label encode categorical features (One Hot probably better here)
 le = preprocessing.LabelEncoder()
-le.fit(obs_df['wind_dir'])
-obs_df['wind_dir'] = le.transform(obs_df['wind_dir'])
+for feature in features_le:
+    le.fit(obs_df[feature])
+    obs_df[feature] = le.transform(obs_df[feature])
 
-obs_df[features] = obs_df[features].astype(float)
+# X = obs_df[features]
+# y = obs_df['air_temp']
 
-X = obs_df[features]
-y = obs_df['air_temp']
+# dt_clf = DecisionTreeRegressor()
+# dt_clf = dt_clf.fit(X, y)
 
-dt_clf = DecisionTreeRegressor()
-dt_clf = dt_clf.fit(X, y)
-
-for clf in [dt_clf]:
-    k_fold = KFold(n_splits=10, shuffle=True)
-    scores = cross_val_score(clf, X, y, cv=k_fold)
-    print(clf,
-          '\nIndividual Scores:', scores,
-          '\nAverage Score:', scores.mean(),
-          '\nNumber of scores used in Average:', len(scores))
+# for clf in [dt_clf]:
+#     k_fold = KFold(n_splits=10, shuffle=True)
+#     scores = cross_val_score(clf, X, y, cv=k_fold)
+#     print(clf,
+#           '\nIndividual Scores:', scores,
+#           '\nAverage Score:', scores.mean(),
+#           '\nNumber of scores used in Average:', len(scores))
--- code/pull_data.py
+++ code/pull_data.py
@@ -1,9 +1,4 @@
 import requests
-import pandas as pd
-from io import StringIO
-
 
 def get_request(url):
-    response = requests.get(url).text
-    df_response = pd.read_json(StringIO(response), orient='index')
-    return pd.DataFrame(df_response.data.observations)
+    return requests.get(url).text
