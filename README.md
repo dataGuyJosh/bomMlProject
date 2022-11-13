9001c84ea4b5a24aab34626a9a43c5221435c02f
Josh Garvey <12745216+dataGuyJosh@users.noreply.github.com>
2022-11-13 15:36:54 +1000

    working on 7 day aggregation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bomMlProject
 Putting together a weather prediction model (ideally with FastAPI) using BoM Weather data.
+
+# URLs
+https://builtin.com/data-science/time-series-forecasting-python
+https://sparkbyexamples.com/pandas/pandas-group-dataframe-rows-list-groupby/
--- code/preprocess.py
+++ code/preprocess.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from pull_data import get_data
 from sklearn import preprocessing
 
@@ -8,19 +9,19 @@ from sklearn.model_selection import cross_val_score, KFold
 
 obs_df = get_data(12)
 
-features_le = ['Date', 'Time of maximum wind gust', 'Direction of maximum wind gust ',
-               '9am wind direction', '3pm wind direction']
-features_ws = ['9am wind speed (km/h)', '3pm wind speed (km/h)']
-
 # replace "Calm" with 0 and convert to int
+features_ws = ['9am wind speed (km/h)', '3pm wind speed (km/h)']
 obs_df[features_ws] = obs_df[features_ws].replace(['Calm'], 0).astype(int)
 
 # label encode categorical features (One Hot probably better here)
+features_le = ['Date', 'Time of maximum wind gust', 'Direction of maximum wind gust ',
+               '9am wind direction', '3pm wind direction']
 le = preprocessing.LabelEncoder()
 for feature in features_le:
     le.fit(obs_df[feature])
     obs_df[feature] = le.transform(obs_df[feature])
 
+# fill null values with 0
 obs_df = obs_df.fillna(0)
 
 features = obs_df.columns.tolist()
@@ -28,28 +29,40 @@ features = obs_df.columns.tolist()
 target = 'Maximum temperature (°C)'
 # target = 'Rainfall (mm)'
 
+features.remove('Date')
 features.remove(target)
 
-X = obs_df[features]
-y = obs_df[target]
+# # shift target down 1 day and drop null values i.e. predict target based on yesterday's entries
+# obs_df['Target: '+target] = obs_df[target].shift(1)
+# obs_df = obs_df.dropna()
 
-dt = DecisionTreeRegressor()
-dt = dt.fit(X, y)
+# group results by 7 days
+target_column = obs_df[target]
+obs_df.Date = obs_df.index/7
+obs_df.Date = obs_df.Date.astype(int)
+obs_df = obs_df.groupby('Date').agg(list)
+obs_df['target'] = target_column
 
-multi_reg = linear_model.LinearRegression()
-multi_reg.fit(X, y)
+# X = obs_df[features]
+# y = obs_df['target']
 
-k_fold = KFold(n_splits=10, shuffle=True)
+# dt = DecisionTreeRegressor()
+# dt = dt.fit(X, y)
 
+# multi_reg = linear_model.LinearRegression()
+# multi_reg.fit(X, y)
 
-def cv_models(models, cv):
-    for model in models:
-        scores = cross_val_score(model, X, y, cv=cv)
-        print(model,
-              '\nIndividual Scores:', scores,
-              '\nAverage Score:', scores.mean(),
-              '\nNumber of scores used in Average:', len(scores), '\n'
-              )
+# k_fold = KFold(n_splits=10, shuffle=True)
 
 
-cv_models([dt, multi_reg], k_fold)
+# def cv_models(models, cv):
+#     for model in models:
+#         scores = cross_val_score(model, X, y, cv=cv)
+#         print(model,
+#               '\nIndividual Scores:', scores,
+#               '\nAverage Score:', scores.mean(),
+#               '\nNumber of scores used in Average:', len(scores), '\n'
+#               )
+
+
+# cv_models([dt, multi_reg], k_fold)
