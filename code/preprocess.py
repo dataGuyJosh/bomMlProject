17c83ae9e54ffad8e7e1e8ab934e0abc44ce94d0
Josh Garvey <12745216+dataGuyJosh@users.noreply.github.com>
2022-11-21 19:42:43 +1000

    flattened columns and removed features in target day

--- code/preprocess.py
+++ code/preprocess.py
@@ -43,24 +43,29 @@ features.remove(target)
 # create week index
 obs_df['week_index'] = (obs_df.index/7).astype(int)
 # store last value as weekly target
-# TODO remove target column (last index) from features
 target_column = obs_df.groupby('week_index')[target].last()
 # create day index
 obs_df['day_index'] = pd.to_datetime(
     obs_df['Date'], format='%Y-%m-%d').dt.weekday
-# pivot table such that rows are weeks and columns are telemetry per day
-obs_df = pd.pivot_table(obs_df, index=['week_index'], columns=['day_index'])
-#
-# ~~~ simplfy pivot table to basic dataframe here ~~~
-#
-obs_df['target'] = target_column
-# drop incomplete weeks (consider alternative solutions such as fillna)
-# TODO would be good to guarantee that latest data always falls in a full week
-obs_df = obs_df.dropna()
-print(obs_df)
 
-# X = obs_df[features]
-# y = obs_df['target']
+# TODO guarantee that latest data always falls in a full week (e.g. reverse the day index)
+obs_df.drop(obs_df[obs_df['day_index'] == 6].index, inplace=True)
+
+# pivot table such that rows are weeks and columns are telemetry per day
+obs_df = pd.pivot_table(obs_df, index=['week_index'], columns=[
+                        'day_index'], aggfunc='last')
+obs_df['target'] = target_column
+
+# drop incomplete weeks (consider alternative solutions such as fillna)
+obs_df = obs_df.dropna()
+
+# flatten columns
+obs_df.columns = [''.join([str(c) for c in c_list])
+                  for c_list in obs_df.columns.values]
+obs_df = obs_df.reset_index().drop(['week_index'], axis=1)
+
+X = obs_df.drop(['target'], axis=1)
+y = obs_df.target
 
 # # Decision Tree
 # dt_model = DecisionTreeRegressor()
@@ -75,17 +80,17 @@ print(obs_df)
 # regr_model = LinearRegression()
 # regr_model.fit(poly_X, y)
 
-# # testing best degree for polynomial, results suggest 5
-# # for i in range(1, 11):
-# #     poly_model = PolynomialFeatures(degree=i)
-# #     poly_X = poly_model.fit_transform(X)
-# #     poly_model.fit(poly_X, y)
-# #     regr_model = LinearRegression()
-# #     regr_model.fit(poly_X, y)
+# # # testing best degree for polynomial, results suggest 5
+# # # for i in range(1, 11):
+# # #     poly_model = PolynomialFeatures(degree=i)
+# # #     poly_X = poly_model.fit_transform(X)
+# # #     poly_model.fit(poly_X, y)
+# # #     regr_model = LinearRegression()
+# # #     regr_model.fit(poly_X, y)
 
-# #     k_fold = KFold(n_splits=10, shuffle=True)
-# #     y_pred = regr_model.predict(poly_X)
-# #     print(i, mean_squared_error(y, y_pred, squared=False))
+# # #     k_fold = KFold(n_splits=10, shuffle=True)
+# # #     y_pred = regr_model.predict(poly_X)
+# # #     print(i, mean_squared_error(y, y_pred, squared=False))
 
 # k_fold = KFold(n_splits=10, shuffle=True)
 
