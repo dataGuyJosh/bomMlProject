30be2fc3b81b7966c503325c526c26f9326f96ec
Josh Garvey <12745216+dataGuyJosh@users.noreply.github.com>
2023-06-30 20:12:33 +1000

    Testing extended prediction range

--- code_2_arima/run.py
+++ code_2_arima/run.py
@@ -28,19 +28,18 @@ bom[ws_cols] = bom[ws_cols].replace(['Calm'], 0).astype(float)
 trn, tst = np.split(bom, [int(0.90*len(bom))])
 
 # Autoregressive Integrated Moving Average (ARIMA)
-model = ARIMA(trn[target_col], order=(range(1, 20), 1, range(1, 20)))
+model = ARIMA(trn[target_col], order=(range(1, 40), 1, range(1, 40)))
 
 # Seasonal Autoregressive Integrated Moving Average (SARIMA)
 # model = ARIMA(trn[target_col], order=(10, 1, 10), seasonal_order=(2,2,2,12))
 
 model = model.fit()
 
-prd = model.get_forecast(len(tst.index))
+prd = model.get_forecast(len(tst.index)*4)
 prd_df = prd.conf_int(alpha=0.05)
 prd_df["Predictions"] = model.predict(
     start=prd_df.index[0], end=prd_df.index[-1])
-prd_df.index = tst.index
-
+# prd_df.index = tst.index
 trn[target_col].plot(color='black', label='Train')
 tst[target_col].plot(color='red', label='Test')
 prd_df['Predictions'].plot(color='green', label='Predictions')
